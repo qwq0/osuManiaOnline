6105ab235e91d899685a76ecb1f9174b39a5f623
qwq0 <82009862+qwq0@users.noreply.github.com>
2024-04-02 13:07:58 +0800

    ud

--- src/canvas.js
+++ src/canvas.js
@@ -7,17 +7,17 @@ canvasElement.style.left = "absolute";
 canvasElement.style.height = "100%";
 canvasElement.style.width = "100%";
 document.body.appendChild(canvasElement);
-canvasElement.width = canvasElement.clientWidth;
-canvasElement.height = canvasElement.clientHeight;
+canvasElement.width = Math.floor(canvasElement.clientWidth * window.devicePixelRatio);
+canvasElement.height = Math.floor(canvasElement.clientHeight * window.devicePixelRatio);
 
 export let canvasContext = canvasElement.getContext("2d");
 
-let oldWidth = 0;
-let oldHeight = 0;
+let oldWidth = canvasElement.width;
+let oldHeight = canvasElement.height;
 function resizeCanvas()
 {
-    let width = canvasElement.clientWidth;
-    let height = canvasElement.clientHeight;
+    let width = Math.floor(canvasElement.clientWidth * window.devicePixelRatio);
+    let height = Math.floor(canvasElement.clientHeight * window.devicePixelRatio);
     if (width != oldWidth || height != oldHeight)
     {
         canvasElement.width = width;
